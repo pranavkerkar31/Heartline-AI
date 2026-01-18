0332207b11ee5d04286a970b05b516fad1741308
Pranav Kerkar <151400162+pranavkerkar31@users.noreply.github.com>
2026-01-18 13:11:20 +0530

    coding

--- myapp/backend/unet.py
+++ myapp/backend/unet.py
@@ -18,3 +18,29 @@ def clean_ecg_pipeline(img_path,model):
 
     # step 3 prepare the image for deep learning model
     orig_w,orig_h = enhanced_gray.shape[1],enhanced_gray.shape[0] # stores the original image size which can be retrieved later
+
+    # step 4 image transformation for U-Net
+    input_tensor = transforms.compose([
+        transforms.ToPILImage(),
+        transforms.Resize((512,512)),
+        transforms.ToTensor(),  
+    ]) (enhanced_gray).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        prediction=model(input_tensor)
+    
+    # step 5 convert output of the model to the image
+    pred_np = prediction.squuze().cpu().numpy()
+    pred_np = (pred_np*255).astype(np.uint8)
+
+    # rebinarization ensures the deblurred lines are pure black
+    _, final_bw = cv2.threshold(pred_np, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    # restore original size using LANCZOS4 to maintain sharpness
+    final_output = cv2.resize(final_bw, (orig_w, orig_h), interpolation=cv2.INTER_LANCZOS4)
+
+    return final_output
+
+source_dir = r"backend/runs/detect/cropped_ecg"
+output_dir = r"backend/runs/detect/cleaned_ecg"
+os.makedirs(output_dir,exist_ok=True)
